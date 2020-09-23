359d5f61725cc42fcb04d2335a3a9bb423f94d63
sanchit2107 <sanchit2107@gmail.com>
2020-09-23 20:55:56 +0530

    Adding NotFound and AlreadyPresent Exceptions

---
+++ src/main/java/com/capgemini/flightmanagement/exceptions/RecordAlreadyPresentException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.capgemini.flightmanagement.exceptions;
+
+/**
+ * @author Sanchit Singhal
+ *
+ */
+public class RecordAlreadyPresentException extends RuntimeException {
+	public RecordAlreadyPresentException(String s) {
+		super(s);
+	}
+
+}
