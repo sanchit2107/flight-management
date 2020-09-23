cdbf3b236d4914c080ebc3cb887ab96db0652396
sanchit2107 <sanchit2107@gmail.com>
2020-09-23 21:31:49 +0530

    Adding Repository annotations and completing DAO class

--- src/main/java/com/capgemini/flightmanagement/dao/AirportDao.java
+++ src/main/java/com/capgemini/flightmanagement/dao/AirportDao.java
@@ -3,10 +3,18 @@
  */
 package com.capgemini.flightmanagement.dao;
 
+import java.math.BigInteger;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.flightmanagement.entity.Airport;
+
 /**
  * @author Sanchit Singhal
  *
  */
-public interface AirportDao {
+@Repository
+public interface AirportDao extends JpaRepository<Airport, BigInteger> {
 
 }
--- src/main/java/com/capgemini/flightmanagement/dao/FlightDao.java
+++ src/main/java/com/capgemini/flightmanagement/dao/FlightDao.java
@@ -1,5 +1,17 @@
 package com.capgemini.flightmanagement.dao;
 
-public interface FlightDao {
+import java.math.BigInteger;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.capgemini.flightmanagement.entity.Flight;
+
+/**
+ * @author Sanchit Singhal
+ *
+ */
+@Repository
+public interface FlightDao extends JpaRepository<Flight, BigInteger>{
 
 }
