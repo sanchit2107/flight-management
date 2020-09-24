34d5bebaaf1527843d2058996ffbadd43ead4df7
sanchit2107 <sanchit2107@gmail.com>
2020-09-24 17:49:20 +0530

    Adding exceptions class related to FlightDetails

---
+++ src/main/java/com/capgemini/flightmanagement/exceptions/FlightDetailsNotFoundException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.capgemini.flightmanagement.exceptions;
+
+/**
+ * @author Sanchit Singhal
+ *
+ */
+public class FlightDetailsNotFoundException extends RuntimeException {
+	public FlightDetailsNotFoundException(String s) {
+		super(s);
+	}
+
+}
