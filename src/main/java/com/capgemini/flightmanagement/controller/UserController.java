711d71e3e3784f9a533df79de8e9e3fcde5a231d
Tarun Kumar <46074444+tarun9777@users.noreply.github.com>
2020-09-26 12:16:41 +0530

    updated

--- src/main/java/com/capgemini/flightmanagement/controller/UserController.java
+++ src/main/java/com/capgemini/flightmanagement/controller/UserController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.flightmanagement.entity.User;
 import com.capgemini.flightmanagement.serviceImpl.UserServiceImpl;
-import com.capgemini.flightmanagement.userException.ExceptionObject;
 
 @RestController
 @CrossOrigin(origins = "*",allowedHeaders = "*")
